26d6c414c63bf606965dd95d5240ea083890f376
iglesias-johnluke <iglesiasjohnluke@gmail.com>
2022-10-14 19:42:20 -0400

    fragments initialized

--- app/src/main/java/com/example/weightliftingapp/MainActivity.kt
+++ app/src/main/java/com/example/weightliftingapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.weightliftingapp
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -22,6 +23,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //open data fragment upon opening app
+        val dataFragment = DataFragment()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.content, dataFragment, "")
+
+
 
 
     }
@@ -37,8 +44,28 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.data_page -> true
-            else -> super.onOptionsItemSelected(item)
+            R.id.data_page -> {
+                val dataFragment = DataFragment()
+                val fragmentTransaction = supportFragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.content, dataFragment, "")
+                fragmentTransaction.commit()
+                true
+            }
+            R.id.browse_page -> {
+                val browseFragment = BrowseWorkoutFragment()
+                val fragmentTransaction = supportFragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.content, browseFragment, "")
+                fragmentTransaction.commit()
+                true
+            }
+            R.id.log_page -> {
+                val logFragment = LogFragment()
+                val fragmentTransaction = supportFragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.content, logFragment, "")
+                fragmentTransaction.commit()
+                true
+            }
+            else -> false
         }
     }
 
