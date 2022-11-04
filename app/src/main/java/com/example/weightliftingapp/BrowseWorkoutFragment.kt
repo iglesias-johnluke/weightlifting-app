e3c8b54f8077adf8c5af8e6bd611e58568e602b2
iglesias-johnluke <iglesiasjohnluke@gmail.com>
2022-11-04 19:18:32 -0400

    firebase signout

--- app/src/main/java/com/example/weightliftingapp/BrowseWorkoutFragment.kt
+++ app/src/main/java/com/example/weightliftingapp/BrowseWorkoutFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.firebase.ui.auth.AuthUI
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,6 +34,7 @@ class BrowseWorkoutFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_browse_workout, container, false)
     }
