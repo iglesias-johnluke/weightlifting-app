46f1792ab4a462b0a37b250e6ae35e2dd2aa2655
iglesias-johnluke <iglesiasjohnluke@gmail.com>
2022-11-19 10:59:00 -0500

    access viewmodel across fragments

--- app/src/main/java/com/example/weightliftingapp/BrowseWorkoutFragment.kt
+++ app/src/main/java/com/example/weightliftingapp/BrowseWorkoutFragment.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.firebase.ui.auth.AuthUI
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+lateinit var sharedViewModel :SharedViewModel
+
 
 /**
  * A simple [Fragment] subclass.
@@ -34,6 +37,8 @@ class BrowseWorkoutFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        sharedViewModel = ViewModelProvider(requireActivity())[SharedViewModel::class.java]
+
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_browse_workout, container, false)
--- app/src/main/java/com/example/weightliftingapp/LogFragment.kt
+++ app/src/main/java/com/example/weightliftingapp/LogFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +21,8 @@ class LogFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    lateinit var sharedViewModel :SharedViewModel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +37,8 @@ class LogFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        sharedViewModel = ViewModelProvider(requireActivity())[SharedViewModel::class.java]
+
         return inflater.inflate(R.layout.fragment_log, container, false)
     }
 
